c44f82ebf04c76b70a8c8ff5c5bbf083667834dc
Yash Bhatt <bhattyash32@yahoo.in>
2022-03-15 10:31:51 +0530

    Keycloak integration and it's packaging

---
+++ user tracking/plugin package/readme.txt
@@ -0,0 +1,7 @@
+How to install .crx file in Chrome Browser:
+
+1. Go to chrome://extensions/ 
+2. Enable Developer mode.
+3. Refresh the page with F5.
+4. Drag and drop the .crx file into the browser chrome://extensions/  page.
+5. Click on 'add' to install.
--- user tracking/plugin/background.js
+++ user tracking/plugin/background.js
@@ -12,11 +12,50 @@ chrome.tabs.onActivated.addListener( function(activeInfo){
     chrome.tabs.get(activeInfo.tabId, function(tab){
         y = tab.url;
         console.log("you are here: "+y);
+		//callBackendApi(y);
     });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
     if (tab.active && change.url) {
         console.log("you are here: "+change.url);
+		//callBackendApi(change.url);
     }
 });
+
+function callBackendApi(y)
+{
+    let data = {
+        "url" : y
+    }
+    let j_data = JSON.stringify(data);
+
+    console.log(j_data)
+    const url = "http://127.0.0.1:5000/";
+    let xhr = new XMLHttpRequest();
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    xhr.open('POST', url, true);
+    xhr.send(j_data);
+
+    xhr.onload = function () {
+        if(xhr.status === 200) {
+            alert("Post successfully created!");
+        }
+    }
+
+    /*alert("function called"+y);
+    $.ajax
+    (
+        {
+            type: "POST",
+            url: "http://127.0.0.1:5000/",
+            data: y,
+            success: function(msg)
+            {
+                console.log("URL Tracked");
+                alert("URL Tracked");
+            }//end function
+        }
+    );//End ajax */
+
+}//end function
