17b7d5de31cd7b828c2fde0e7ee5e5c555b240b0
sunny <sunny@atcults.com>
2022-06-11 14:40:07 +0530

    Adding sample for process monitoring and IPC NamedPipes

---
+++ MonitorTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Management;
+
+namespace MonitorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementEventWatcher startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived += new EventArrivedEventHandler(StartWatchEventArrived);
+            startWatch.Start();
+            ManagementEventWatcher stopWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            stopWatch.EventArrived += new EventArrivedEventHandler(StopWatchEventArrived);
+            stopWatch.Start();
+            Console.WriteLine("Press any key to exit");
+            while (!Console.KeyAvailable) System.Threading.Thread.Sleep(50);
+            startWatch.Stop();
+            stopWatch.Stop();
+        }
+
+        static void StopWatchEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            var processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+
+            if (processName.ToLower().Contains("notepad"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine("Process stopped: {0}", processName);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void StartWatchEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            var processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+
+            if (processName.ToLower().Contains("notepad"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine("Process started: {0}", e.NewEvent.Properties["ProcessName"].Value);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
