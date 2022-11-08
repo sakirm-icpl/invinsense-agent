439a4582071ff17d30e0e84b9c1731185d1d87f1
Sunny <sunny@atcults.com>
2022-11-08 17:45:53 +0530

    Making custom action as separate project.

---
+++ CustomActions/CustomActions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace ToolManager
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CheckSessionParameters(Session session)
+        {
+            session.Log("Hello World! from Install");
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult RemoveTools(Session session)
+        {
+            session.Log("Hello World! from Uninstall");
+            return ActionResult.Success;
+        }
+    }
+}
