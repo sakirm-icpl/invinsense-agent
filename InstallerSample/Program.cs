d7f2f2d66c413a4b014096e1a5a27b84e069abff
Vaibhavi Pandya <103485015+vaibhavipinfopercept@users.noreply.github.com>
2022-11-29 15:22:55 +0530

    Remove wrapper class

--- InstallerSample/Program.cs
+++ InstallerSample/Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Management;
 using System.ServiceProcess;
 
@@ -38,8 +37,6 @@ namespace InstallerSample
                         //this program may not have a name property
                     }
                 }*/
-
-
                 logger.Info("Stopping Invinsense service");
 
                 var service = new ServiceController("Invinsense Agent");
@@ -49,7 +46,7 @@ namespace InstallerSample
 
                 logger.Info("Service is stopped");
 
-               // Console.ReadLine();
+                // Console.ReadLine();
 
                 /*
                 foreach (var process in Process.GetProcesses())
@@ -57,7 +54,6 @@ namespace InstallerSample
                     logger.Info($"Process: {process.Id}, Name: {process.ProcessName}");
                 }
                 */
-
                 logger.Info("Trying to stop IvsTray");
 
                 foreach (var process in Process.GetProcessesByName("IvsTray"))
@@ -66,11 +62,25 @@ namespace InstallerSample
                     process.Kill();
                 }
 
-               // Console.ReadLine();
+                logger.Info("Removing Service");
+                var exePath ="C:\\Windows\\System32\\sc.exe";
+                Process installerProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                         FileName = exePath,
+                         Arguments = "delete \"Invinsense Agent\"",
+                         WindowStyle = ProcessWindowStyle.Hidden,
+                         CreateNoWindow = true
+                     }
+                 };
 
+                installerProcess.Start();
+                logger.Info("Remove Service Successfully");
+                
                 logger.Info("Uninstalling Invinsense Agent");
 
-                var unInstallStatus = UninstallWrapper.UninstallProgram("Invinsense Single Agent 3.0");
+                var unInstallStatus = UninstallProgram("Invinsense Single Agent 3.0");
 
                 logger.Info($"Agent uninstall status: {unInstallStatus}");
 
@@ -83,6 +93,7 @@ namespace InstallerSample
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
+                //logger.Error(ex.StackTrace);
             }
             finally
             {
@@ -90,12 +101,102 @@ namespace InstallerSample
             }
 
             Console.WriteLine("Done.");
+        }     
 
-           // Console.ReadLine();
+        public static bool UninstallProgram(string ProgramName)
+        {
+            try
+            {
+                ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_Product WHERE Name = '" + ProgramName + "'");
 
+                foreach (ManagementObject mo in mos.Get())
+                {
+                    try
+                    {
+                        if (mo["Name"].ToString() == ProgramName)
+                        {
+                            object hr = mo.InvokeMethod("Uninstall", null);
+
+                            Console.WriteLine($"Uninstall invoke return code: {hr}");
+
+                            return (bool)hr;
+                        }
+                    }
+                    catch
+                    {
+                        //this program may not have a name property, so an exception will be thrown
+                    }
+                }
+
+                //was not found...
+                return false;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
+        void UnInstallPackage(string packageName)
+        {
+            string searchString = $"SELECT * FROM Win32_Product WHERE Name LIKE '{packageName}'";
 
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(searchString);
 
+            foreach (ManagementObject mo in mos.Get())
+            {
+                // Will return Name, IdentifyingNumber and Version
+                try
+                {
+                    Console.WriteLine("Properties");
+                    foreach (var item in mo.Properties)
+                    {
+                        try
+                        {
+                            Console.WriteLine(item.Name + "-" + item.Value.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error in property: {ex.Message}");
+                        }
+                    }
+
+                    Console.WriteLine("System Properties");
+                    foreach (var item in mo.SystemProperties)
+                    {
+                        try
+                        {
+                            Console.WriteLine(item.Name + "-" + item.Value.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error in property: {ex.Message}");
+                        }
+                    }
+
+                    if (mo["Name"].ToString().Contains(packageName))
+                    {
+                        //object arp = mo.InvokeMethod("Uninstall", null);
+                        Process process = new Process();
+                        process.StartInfo.FileName = "msiexec.exe";
+                        process.StartInfo.Arguments = "/X" + mo["IdentifyingNumber"].ToString() + " /q";
+                        process.StartInfo.UseShellExecute = false;
+                        process.StartInfo.RedirectStandardInput = true;
+                        process.StartInfo.RedirectStandardOutput = true;
+                        process.StartInfo.RedirectStandardError = true;
+                        process.StartInfo.CreateNoWindow = false;
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //this program may not have a name property, so an exception will be thrown
+                    //handle your exception here
+                    Console.WriteLine(ex.Message);
+                }            
+            }
+        }
         private static void SetStartup(string name, string path, bool addOrRemove)
         {
             RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
