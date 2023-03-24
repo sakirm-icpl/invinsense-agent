a407ad4a0c934709a5e9dc8f497d9b486b9e9155
Sanchania Darshna <darshanas@infopercept.com>
2023-03-24 17:33:01 +0530

    Separate project for Osquery installation and Wazuh installation.

--- WazuhInstall/Wazuh.cs
+++ WazuhInstall/Wazuh.cs
@@ -1,68 +1,54 @@
-﻿using Common.Utils;
-using Serilog;
-using System;
+﻿using Serilog;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
+using Common.Utils;
 using System.Xml;
-using Common.Persistance;
-using ToolManager.MsiWrapper;
-using System.IO;
 
-namespace ToolManager.AgentWrappers
+namespace Wazuh
 {
-    public static class WazuhWrapper
+    public static class Wazuh
     {
-        private static readonly ILogger _logger = Log.ForContext(typeof(WazuhWrapper));
-        
-        public static int Verify(bool isInstall = false)
+        private static readonly ILogger _logger = Log.ForContext(typeof(Wazuh));
+        public static void Main(string[] args)
         {
             try
             {
-
                 ServiceController ctl = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "WazuhSvc");
-
                 if (ctl != null)
                 {
                     _logger.Information($"END_POINT_DETECTION_AND_RESPONSE found with status: {ctl.Status}");
-                    return 0;
-                }
 
-                if (ctl == null && !isInstall)
+                }
+                if (ctl == null)
                 {
                     _logger.Information("END_POINT_DETECTION_AND_RESPONSE not found and set for skip.");
-                    return -1;
+
                 }
                 _logger.Information("END_POINT_DETECTION_AND_RESPONSE not found and set for skip.");
 
-                if (!MsiPackageWrapper.IsMsiExecFree(TimeSpan.FromMinutes(5)))
-                {
-                    _logger.Information("MSI Installer is not free.");
-                    return 1618;
-                }
                 _logger.Information("END_POINT_DETECTION_AND_RESPONSE installation is ready");
 
-                var msiPath = CommonUtils.GetAbsoletePath("..\\artifacts\\wazuh\\wazuh-agent-4.3.10-1.msi");
+                var msiPath = CommonUtils.GetAbsoletePath("D:\\invinsense-agent\\artifacts\\wazuh\\wazuh-agent-4.3.10-1.msi");
 
                 var logPath = CommonUtils.DataFolder + "\\wazuhInstall.log";
 
                 _logger.Information($"Wazuh's msiPath {msiPath}");
                 _logger.Information($"Wazuh's logPath {logPath}");
 
-                var managerIp = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "MANAGER_ADDR");
-                var registrationIp = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "REGISTRATION_SERVER_ADDR");
-                var agentGroup = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "AGENT_GROUP");
+                //var managerIp = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "MANAGER_ADDR");
+                //var registrationIp = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "REGISTRATION_SERVER_ADDR");
+                //var agentGroup = ToolRepository.GetPropertyByName(ToolName.EndpointDecetionAndResponse, "AGENT_GROUP");
 
-                _logger.Information($"Wazuh's ManagerIp {managerIp}");
-                _logger.Information($"Wazuh's RegistrationIP {registrationIp}");
-                _logger.Information($"Wazuh's AgentGroup {agentGroup}");
+                // _logger.Information($"Wazuh's ManagerIp {managerIp}");
+                //_logger.Information($"Wazuh's RegistrationIP {registrationIp}");
+                //_logger.Information($"Wazuh's AgentGroup {agentGroup}");
 
                 Process installerProcess = new Process
                 {
                     StartInfo = new ProcessStartInfo
                     {
                         FileName = "msiexec",
-                        Arguments = $"/I \"{msiPath}\" /QN /l*vx \"{logPath}\" ACCEPTEULA=1 ALLUSERS=1 WAZUH_MANAGER=\"{managerIp}\" WAZUH_REGISTRATION_SERVER=\"{registrationIp}\" WAZUH_AGENT_GROUP=\"{agentGroup}\"",
+                        Arguments = $"/I \"{msiPath}\" /QN /l*vx \"{logPath}\" ACCEPTEULA=1 ALLUSERS=1 WAZUH_MANAGER=\"172.17.14.101\" WAZUH_REGISTRATION_SERVER=\"172.17.14.101\" WAZUH_AGENT_GROUP=\"default\"",
                         WindowStyle = ProcessWindowStyle.Hidden,
                         CreateNoWindow = true,
                         WorkingDirectory = CommonUtils.RootFolder
@@ -77,7 +63,7 @@ namespace ToolManager.AgentWrappers
 
                 _logger.Information("END_POINT_DETECTION_AND_RESPONSE Installation started...");
 
-                installerProcess.WaitForExit();
+                installerProcess.WaitForExit(200000);
 
                 if (installerProcess.ExitCode == 0)
                 {
@@ -85,11 +71,14 @@ namespace ToolManager.AgentWrappers
 
                     _logger.Information("Copying local_internal_options.conf file to wazuh installed directory");
 
-                    System.IO.File.Copy(CommonUtils.GetAbsoletePath("..\\artifacts\\local_internal_options.conf"), "C:\\Program Files (x86)\\ossec-agent\\local_internal_options.conf", true);
+                    System.IO.File.Copy(CommonUtils.GetAbsoletePath("D:\\invinsense-agent\\artifacts\\wazuh\\local_internal_options.conf"), "C:\\Program Files (x86)\\ossec-agent\\local_internal_options.conf", true);
+
+                    Thread.Sleep(1000);
 
                     _logger.Information("Enable osquery for END_POINT_DETECTION_AND_RESPONSE");
 
                     var confFile = "C:\\Program Files (x86)\\ossec-agent\\ossec.conf";
+
                     XmlDocument document = new XmlDocument();
                     document.Load(confFile);
                     XmlNodeList osQueryDisableNodeItems = document.SelectNodes("/ossec_config/wodle[@name='osquery']/disabled");
@@ -117,24 +106,19 @@ namespace ToolManager.AgentWrappers
                     {
                         ctl.Start();
                     }
-
-                    return 0;
                 }
                 else
                 {
                     _logger.Information($"END_POINT_DETECTION_AND_RESPONSE installation fault: {installerProcess.ExitCode}");
-                    return installerProcess.ExitCode;
                 }
             }
             catch (Exception ex)
             {
                 _logger.Error($"{ex.Message}");
-                return 1;
             }
         }
-
         private static void InstallerProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        { 
+        {
             _logger.Information($"END_POINT_DETECTION_AND_RESPONSE installation error data: {e.Data}");
         }
 
@@ -146,43 +130,6 @@ namespace ToolManager.AgentWrappers
         private static void InstallerProcess_Exited(object sender, EventArgs e)
         {
             _logger.Information("END_POINT_DETECTION process exited.");
-        }
-
-        public static int Remove()
-        {
-            try
-            {
-                bool status = false;
-                ServiceController ctl = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "WazuhSvc");
-
-                if (ctl == null)
-                {
-                    _logger.Information("END_POINT_DETECTION_AND_RESPONSE not found. Skipping...");
-                    return -1;
-                }
-                _logger.Information("END_POINT_DETECTION_AND_RESPONSE found. Preparing uninstallation");
-
-                if (!MsiPackageWrapper.IsMsiExecFree(TimeSpan.FromMinutes(5)))
-                {
-                    _logger.Information("MSI Installer is not free.");
-                    return 1618;
-                }
-                _logger.Information("END_POINT_DETECTION_AND_RESPONSE Uninstallation is ready");
-
-                //Checking if file is exists or not
-                if(Verify(true) == 0)
-                {
-                        status = MsiPackageWrapper.Uninstall("Wazuh Agent");
-                }
-                
-                return status ? 0 : 1;
-
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex.Message,new { WazuhError = $"{ex.Message}" });
-                return 1;
-            }
         }
     }
 }
