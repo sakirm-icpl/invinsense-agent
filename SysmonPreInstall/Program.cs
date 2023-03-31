1f4b0a35181cfbe4fc7917553d15386807ac041c
Sanchania Darshna <darshanas@infopercept.com>
2023-03-31 16:16:17 +0530

    New project for SysmonPreInstall.

---
+++ SysmonPreInstall/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace SysmonPreInstall
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            ServiceController ctl = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "Sysmon64");
+            if (ctl != null) 
+            {
+                // Get the path to the osquery executable
+                string sysmonPath = @"C:\Windows\Sysmon64.exe";
+
+                // Use FileVersionInfo to get the name and version of the executable
+                FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(sysmonPath);
+
+                // Print the name and version
+                var productName = fileVersionInfo.ProductName;
+
+                var productVersion = fileVersionInfo.ProductVersion;
+                
+                Console.WriteLine(fileVersionInfo.FileVersion);
+            }
+        }
+    }
+}
