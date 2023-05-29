00952dca1d7a3b3de8098702f2dfd267bdb858cf
Sunny <sunnym@infopercept.com>
2023-05-29 17:16:03 +0530

    Adding tests for CommonUtils

--- Common/Utils/CommonUtils.cs
+++ Common/Utils/CommonUtils.cs
@@ -11,9 +11,13 @@ namespace Common.Utils
 
         public static string RootFolder => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-        public static string GetAbsoletePath(string path) => Path.GetFullPath(new Uri(Path.Combine(RootFolder, path)).LocalPath);
+        public static string ConstructFromRoot(string path) => Path.GetFullPath(new Uri(Path.Combine(RootFolder, path)).LocalPath);
 
         public static string ConfigFolder => Path.Combine(DataFolder, "configs");
+
+        public static string ArtifactsFolder => Path.Combine(DataFolder, "artifacts");
+
+        public static string LogsFolder => Path.Combine(DataFolder, "logs");
 
         public static string DataFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Infopercept");
 
---
+++ CommonTests/CommonUtilTests.cs
@@ -0,0 +1,31 @@
+using Common.Utils;
+
+namespace CommonTests
+{
+    public class CommonUtilTests
+    {
+        [Fact]
+        public void ShouldResolveProgramData()
+        {
+            Assert.Equal("C:\\ProgramData\\Infopercept", CommonUtils.DataFolder);
+        }
+
+        [Fact]
+        public void ShouldMapArtifactsFolder()
+        {
+            Assert.Equal("C:\\ProgramData\\Infopercept\\artifacts", CommonUtils.ArtifactsFolder);
+        }
+
+        [Fact]
+        public void ShouldMapConfigFolder()
+        {
+            Assert.Equal("C:\\ProgramData\\Infopercept\\configs", CommonUtils.ConfigFolder);
+        }
+
+        [Fact]
+        public void ShouldMapLogsFolder()
+        {
+            Assert.Equal("C:\\ProgramData\\Infopercept\\logs", CommonUtils.LogsFolder);
+        }
+    }
+}
